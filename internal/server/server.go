113b19308ee5c11dee999b8ffa5f6b450a980f05
Artem <burenotti@gmail.com>
2023-04-11 15:27:51 +0300

    ✨ Implemented Listen grpc method

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"github.com/practice-sem-2/notification-service/internal/pb"
 	"github.com/practice-sem-2/notification-service/internal/usecase"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type NotificationsServer struct {
@@ -17,5 +19,19 @@ func NewNotificationServer(ucases *usecase.UseCase) *NotificationsServer {
 }
 
 func (s *NotificationsServer) Listen(r *pb.ListenRequest, server pb.Notifications_ListenServer) error {
-	panic("Unimplemented")
+	user, err := s.ucases.Verifier.GetUser(server.Context())
+
+	if err != nil {
+		return status.Error(codes.Unauthenticated, err.Error())
+	}
+
+	listener := s.ucases.Notifications.Listen(user.Username)
+	for notification := range listener.Notifications() {
+		err := server.Send(notification)
+		if err != nil {
+			listener.Detach()
+			return err
+		}
+	}
+	return nil
 }
